624ed5499dddd4d8873da8517fedc4ceb8d95f46
Dr_Kee <derec30240@163.com>
2024-12-07 20:49:22 +0800

    Reconstruct the HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,7 +3,9 @@ import 'package:get/get.dart';
 
 import 'about_page.dart';
 import '../controllers/global_controller.dart';
+import 'notes_page.dart';
 import 'settings_page.dart';
+import 'tasks_page.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
@@ -12,79 +14,10 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context)
-        .textTheme
-        .apply(displayColor: Theme.of(context).colorScheme.onPrimary);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tiny Note'),
-        actions: [
-          Obx(() {
-            // Show the Folder button only in the Notes screen
-            return globalController.selectedHomeIndex.value == 0
-                ? IconButton(
-                    tooltip: 'Folders',
-                    //TODO: Enable to create and manage folders.
-                    onPressed: () {},
-                    icon: const Icon(Icons.folder_outlined),
-                  )
-                : Container();
-          }),
-          IconButton(
-            tooltip: 'Search',
-            //TODO: Enable to search for notes and tasks.
-            onPressed: () {},
-            icon: const Icon(Icons.search),
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Tiny Note',
-                    style: textTheme.headlineLarge!.copyWith(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    'Seize your every inspiration',
-                    softWrap: true,
-                    textAlign: TextAlign.center,
-                    style: textTheme.labelLarge!.copyWith(
-                      color: Theme.of(context).colorScheme.onPrimary,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text('Settings'),
-              onTap: () {
-                Get.to(() => SettingsPage());
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.info),
-              title: const Text('About'),
-              onTap: () {
-                Get.to(() => AboutPage());
-              },
-            ),
-          ],
-        ),
-      ),
-      //TODO: Fill the body with meaningful things.
-      body: const Placeholder(),
+      appBar: _appBar(),
+      drawer: _drawer(context),
+      body: SafeArea(child: Obx(() => _body())),
       bottomNavigationBar: Obx(() {
         return NavigationBar(
           selectedIndex: globalController.selectedHomeIndex.value,
@@ -103,6 +36,92 @@ class HomePage extends StatelessWidget {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+
+  PreferredSizeWidget _appBar() {
+    return AppBar(
+      title: const Text('Tiny Note'),
+      actions: [
+        Obx(() {
+          // Show the Folder button only in the Notes screen
+          return globalController.selectedHomeIndex.value == 0
+              ? IconButton(
+                  tooltip: 'Folders',
+                  //TODO: Enable to create and manage folders.
+                  onPressed: () {},
+                  icon: const Icon(Icons.folder_outlined),
+                )
+              : Container();
+        }),
+        IconButton(
+          tooltip: 'Search',
+          //TODO: Enable to search for notes and tasks.
+          onPressed: () {},
+          icon: const Icon(Icons.search),
+        ),
+      ],
+    );
+  }
+
+  Widget _drawer(BuildContext context) {
+    final textTheme = Theme.of(context)
+        .textTheme
+        .apply(displayColor: Theme.of(context).colorScheme.onPrimary);
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Tiny Note',
+                  style: textTheme.headlineLarge!.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Seize your every inspiration',
+                  softWrap: true,
+                  textAlign: TextAlign.center,
+                  style: textTheme.labelLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            onTap: () {
+              Get.to(() => SettingsPage());
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.info),
+            title: const Text('About'),
+            onTap: () {
+              Get.to(() => AboutPage());
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _body() {
+    switch (globalController.selectedHomeIndex.value) {
+      case 1:
+        return TasksPage();
+      case 0:
+      default:
+        return NotesPage();
+    }
   }
 }
 
