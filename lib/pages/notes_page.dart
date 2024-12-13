2603e2b0ec0f1090776b450a2319137f7441075a
Dr_Kee <derec30240@163.com>
2024-12-13 23:30:01 +0800

    Change the appearance of note_page.

--- lib/pages/notes_page.dart
+++ lib/pages/notes_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 import 'package:tiny_note/controllers/global_controller.dart';
 import 'package:tiny_note/controllers/notes_controller.dart';
+import 'package:tiny_note/models/note.dart';
 import 'package:tiny_note/pages/note_detail_page.dart';
 
 class NotesPage extends StatelessWidget {
@@ -30,35 +32,22 @@ class NotesPage extends StatelessWidget {
               itemCount: notesController.notes.length,
               itemBuilder: (context, index) {
                 //FIXME: If the `notesSortOrder` is `dateLastEdited`, when finished edit, it may go back to another NoteDetailPage. Try not to use `index` to index the note, or maybe other solutions.
-                return ListTile(
-                  onTap: () {
-                    Get.to(() => NoteDetailPage(index: index));
-                  },
-                  title: Text(
-                    notesController.notes[index].title,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                    maxLines: 1,
-                  ),
-                  subtitle: Text(
-                    notesController.notes[index].content,
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                  ),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
+                Note note = notesController.notes[index];
+                return Slidable(
+                  key: ValueKey(index),
+                  endActionPane: ActionPane(
+                    motion: const DrawerMotion(),
                     children: [
-                      Text(DateFormat('yyyy-MM-dd HH:mm:ss').format(
-                          globalController.noteSortOrder.value == 'dateCreated'
-                              ? notesController.notes[index].dateCreated
-                              : notesController.notes[index].dateLastEdited)),
-                      IconButton(
-                        onPressed: () {
+                      SlidableAction(
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                        autoClose: true,
+                        onPressed: (content) {
                           Get.defaultDialog(
                             title: 'Delete Note',
                             middleText: 'Confirm to delete this note?',
                             textConfirm: 'Yes',
                             textCancel: 'No',
-                            buttonColor: Theme.of(context).colorScheme.primary,
                             onConfirm: () {
                               notesController.deleteNote(index);
                               Get.back();
@@ -77,13 +66,46 @@ class NotesPage extends StatelessWidget {
                             },
                           );
                         },
-                        icon: const Icon(Icons.delete),
-                      )
+                        icon: Icons.delete,
+                        label: 'Delete',
+                        borderRadius: BorderRadius.circular(15),
+                      ),
                     ],
                   ),
+                  child: noteCard(context, note, index),
                 );
               },
             ),
           ));
+  }
+
+  Widget noteCard(BuildContext context, Note note, int index) {
+    return Card(
+      child: ListTile(
+        onTap: () {
+          Get.to(() => NoteDetailPage(index: index));
+        },
+        title: Text(
+          note.title,
+          style: const TextStyle(fontWeight: FontWeight.bold),
+          maxLines: 1,
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              note.content,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+            ),
+            const Divider(),
+            Text(DateFormat('yyyy-MM-dd HH:mm:ss').format(
+                globalController.noteSortOrder.value == 'dateCreated'
+                    ? note.dateCreated
+                    : note.dateLastEdited)),
+          ],
+        ),
+      ),
+    );
   }
 }
