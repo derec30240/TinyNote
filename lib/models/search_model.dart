063350097f3b7ce0118fd35011a23e4c330b6251
Dr_Kee <derec30240@163.com>
2024-12-16 21:16:32 +0800

    Temporarily fixed the search function.

--- lib/models/search_model.dart
+++ lib/models/search_model.dart
@@ -1,118 +1,100 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_connect.dart';
+
 import 'package:tiny_note/controllers/notes_controller.dart';
-import 'package:tiny_note/pages/home_page.dart';
+import 'package:tiny_note/pages/note_detail_page.dart';
 
-
-
-class DataSearch extends SearchDelegate<String>{
-
-
-  //get title at here
-  final alltitle  = getAlltitle();
-  // = NotesController().notes;
-  /*final alltitle= [
-    "Null",
-    "Nul",
-    ];*/
-  
-  final recentSearch = [
-    "Null",
-    "Nul",
-    ];
-  
-  
-  
+class DataSearch extends SearchDelegate<String> {
+  NotesController get _notesController => Get.find();
 
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
       IconButton(
-        onPressed: (){
+        onPressed: () {
           query = '';
-          print(NotesController().notes);
-        }, 
+        },
         icon: const Icon(Icons.clear),
-        )
+      )
     ];
   }
 
   @override
-  //close the srearch collum
   Widget? buildLeading(BuildContext context) {
     return IconButton(
-      onPressed:(){
-        //Get.to(const MyHomePage(title: 'Flutter Demo Home Page'));
-        Navigator.pop( context);
-      }, 
+      onPressed: () {
+        Get.back();
+      },
       icon: AnimatedIcon(
         icon: AnimatedIcons.menu_arrow,
-        progress: transitionAnimation
+        progress: transitionAnimation,
       ),
     );
   }
 
   @override
   Widget buildResults(BuildContext context) {
-    return Center(
-      child: Text(query),
+    final matchingNoteTitles = _notesController.noteTitles
+        .where((title) => title.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    return ListView.builder(
+      itemCount: matchingNoteTitles.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(matchingNoteTitles[index]),
+          onTap: () {},
+        );
+      },
     );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     final suggestionList = query.isEmpty
-      ? recentSearch 
-      : alltitle.where((element) => element.startsWith(query)).toList();
+        ? []
+        : _notesController.noteTitles
+            .where(
+                (title) => title.toLowerCase().startsWith(query.toLowerCase()))
+            .toList();
     return ListView.builder(
-      itemBuilder: (context, index) => ListTile(
-        leading: const Icon(Icons.note),
-        onTap: (){
-          showResults( context);
-        },
-        title: TextButton(
-          onPressed: (){
-            //
-            Navigator.pop( context);
-          },
-          //matched text, ues bold
-          child: Text.rich(
-            TextSpan(
-              text: suggestionList[index].substring(0,query.length),
-              style: const TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                
-              ),
-              children: [
-                TextSpan(
-                  //the text not matched, so the font colot is grey
-                  text: suggestionList[index].substring(query.length),
-                  style: TextStyle(
-                    color: Colors.grey
-                  ),
-                  recognizer: TapGestureRecognizer()
-                    ..onTap =(){
-                     // Get.to(Placeholder());
-                    }
-                )
-              ]
-            ),
-          textAlign: TextAlign.left, 
-          )
-        ),
-      ),
       itemCount: suggestionList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: const Icon(Icons.text_snippet),
+          onTap: () {
+            query = suggestionList[index];
+            showResults(context);
+          },
+          title: TextButton(
+            onPressed: () {
+              Get.to(() => NoteDetailPage(
+                  index: _notesController.notes.indexWhere((note) =>
+                      note.title?.toLowerCase() ==
+                      suggestionList[index].toLowerCase())));
+            },
+            //matched text, ues bold
+            child: Text.rich(
+              textAlign: TextAlign.left,
+              TextSpan(
+                text: suggestionList[index].substring(0, query.length),
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+                children: [
+                  TextSpan(
+                    //the text not matched, so the font colot is grey
+                    text: suggestionList[index].substring(query.length),
+                    style: TextStyle(color: Colors.grey),
+                    recognizer: TapGestureRecognizer()..onTap = () {},
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
-}
-
-List<String> getAlltitle() {
-  List<String> gettitle = List.empty();
-  for (var element in NotesController().notes) {
-    gettitle.add(element.title);
-  }
-  return gettitle;
 }
